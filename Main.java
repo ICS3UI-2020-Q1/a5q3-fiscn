f580112f51114294fa72a267a7e18311f5cbabb6
fiscn <fiscn2709@wrdsb.ca>
2020-10-06 14:04:54 +0000

    Finished A5 Q3 program

--- Main.java
+++ Main.java
@@ -1,6 +1,8 @@
+import java.util.Scanner; 
+
 /**
- *
- * @author 
+ * program that will take a positive integer and compute the factorial of that integer.
+ * @author Neil Fischer
  */
 public class Main {
 
@@ -9,7 +11,23 @@ public class Main {
    * @param args the command line arguments
    */
   public static void main(String[] args) {
-    // TODO code application logic here
+    // Create scanner for user input 
+    Scanner input = new Scanner(System.in); 
+
+    //prompt the user to enter in a integer 
+    System.out.println("Please enter an integer to calculate the factorial of"); 
+
+    //declare variables 
+    int userNM = input.nextInt(); 
+    int answerNM = 1; 
+
+    //create for loop for the factorial of the integer 
+    for(int count = userNM; count >= 1; count--){
+      answerNM = answerNM * count; 
+    } 
+    System.out.println(userNM + "!" + "= " + answerNM); 
+
+
     
   }
 }
